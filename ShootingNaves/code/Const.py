0046c92d789be39d3a71d2fce3214dfbcb04bb4d
DanielCont <danielpfcont@gmail.com>
2024-07-01 15:40:18 -0300

    Criação e movimentação de entidades

--- ShootingNaves/code/Const.py
+++ ShootingNaves/code/Const.py
@@ -1,2 +1,22 @@
+# C
+COLOR_ORANGE = (155, 128, 50)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 128)
+
+# E
+ENTITY_SPEED = {'Level1Bg0': 0,
+                'Level1Bg1': 1,
+                'Level1Bg2': 2,
+                'Level1Bg3': 3,
+                'Level1Bg4': 0,
+                }
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOP',
+               'NEW GAME 2P - COMPETITIVE',
+               'EXIT')
+
+# W
 WIN_WIDTH = 1300
 WIN_HEIGHT = 731
--- ShootingNaves/code/Menu.py
+++ ShootingNaves/code/Menu.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
 import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -14,21 +16,48 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
+        # Música
         pygame.mixer_music.load('./asset/Menu.flac')
         pygame.mixer_music.play(-1)
         pygame.mixer.music.set_volume(0.1)
+        menu_option = 0
 
         while True:
+            # Criação da janela e título
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Shooting", (155, 128, 0), ((WIN_WIDTH / 2), 100))
-            self.menu_text(50, "Naves", (155, 128, 0), ((WIN_WIDTH / 2), 50)) #
+            self.menu_text(50, "Shooting", COLOR_ORANGE, ((WIN_WIDTH / 2), 130))
+            self.menu_text(50, "Naves", COLOR_ORANGE, ((WIN_WIDTH / 2), 180))
+
+            # Mostra as opções do menu e formata o texto
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 300 + 50 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 300 + 50 * i))
+
             pygame.display.flip()
 
-            # Fecha o jogo
+            # Verifica eventos - Interação
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT:  # Fecha o jogo
                     pygame.quit()
-                    quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:  # Se uma tecla for pressionada
+                    if event.key == pygame.K_DOWN:  # Seta para BAIXO
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+
+                    if event.key == pygame.K_UP:  # Seta para CIMA
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+
+                    if event.key == pygame.K_RETURN:  # Tecla ENTER
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
